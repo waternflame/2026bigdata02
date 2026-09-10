39fe954c51dfa17cf01ec83c6c3eb4167effabbb
waternflame <naoireireze@gmail.com>
2026-09-10 11:47:21 +0900

    week02 v0.2

--- week02.py
+++ week02.py
@@ -2,9 +2,10 @@ import numpy as np
 import pandas as pd
 
 df1 = pd.read_csv("./bike.csv")
-#print(df1.head())
-#print(df1.info())
-#print(df1['temp'])
-print(df1.loc[df1['season'] != 1])
-#print(df1.iloc[3:8,3:7]) #3열부터 8열까지, 3행부터 6행까지 출력
-#print(df1.loc[3:7,'workingday':'atemp']
+#print(df1.loc[df1['season'] == 3, ['season', 'humidity', 'windspeed', 'casual' ,'registered']])
+#print(df1.loc[df1['season'] == 3, 'humidity':'registered'])
+#print(df1['weather'].value_counts())
+#print(df1.loc[(df1['season'] != 4)&(df1['weather'] != 1)])
+#temp = df1.loc[(df1['season'] != 4)&(df1['weather'] != 1)]
+#print(temp['weather']==1)
+print(df1.loc[3, 'humidity'])
