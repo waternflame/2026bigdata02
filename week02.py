f2c013d783e1f4f5052842e7283f7f02dd83918d
waternflame <naoireireze@gmail.com>
2026-09-10 12:29:23 +0900

    week02 v0.3

--- week02.py
+++ week02.py
@@ -2,10 +2,11 @@ import numpy as np
 import pandas as pd
 
 df1 = pd.read_csv("./bike.csv")
-#print(df1.loc[df1['season'] == 3, ['season', 'humidity', 'windspeed', 'casual' ,'registered']])
-#print(df1.loc[df1['season'] == 3, 'humidity':'registered'])
-#print(df1['weather'].value_counts())
-#print(df1.loc[(df1['season'] != 4)&(df1['weather'] != 1)])
-#temp = df1.loc[(df1['season'] != 4)&(df1['weather'] != 1)]
-#print(temp['weather']==1)
-print(df1.loc[3, 'humidity'])
+#print(df1.info())
+#print(df1.select_dtypes(include="float")) float 데이터 값만 출력
+#print(df1.select_dtypes(exclude="float")) float 데이터 값을 제외하고 출력
+#print(df1.filter(regex="d..y"))
+#print(df1.filter(items=['windspeed', 'season']))
+df2 = df1.set_index('datetime')
+#print(df2)
+print(df2.filter(like='00:00:00', axis=0))
